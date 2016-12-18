fc2cdfcb020a7066c240ff6298ecff5bdd2efc35
maor <maor@logz.io>
2016-12-18 16:35:42 +0200

    Add log4net sample application

---
+++ sample-applications/LogzioLog4netSampleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+using log4net;
+using log4net.Config;
+
+namespace LogzioLog4netSampleApplication
+{
+	class Program
+	{
+		static void Main()
+		{
+			XmlConfigurator.Configure();
+
+			var logger = LogManager.GetLogger("GreetingsLogger");
+			logger.Info("Hello to you");
+			Thread.Sleep(TimeSpan.FromSeconds(10));
+		}
+	}
+}
