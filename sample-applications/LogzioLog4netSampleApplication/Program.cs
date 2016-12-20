e86c2029d1ab0325afc3f7b20dbfcc4f9be77767
maor <maor@logz.io>
2016-12-20 16:03:40 +0200

    Update log4net app minorly

--- sample-applications/LogzioLog4netSampleApplication/Program.cs
+++ sample-applications/LogzioLog4netSampleApplication/Program.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Threading;
-using log4net;
+﻿using log4net;
 using log4net.Config;
 
 namespace LogzioLog4netSampleApplication
 {
-	class Program
+	public class Program
 	{
 		static void Main()
 		{
 			XmlConfigurator.Configure();
 
 			var logger = LogManager.GetLogger("GreetingsLogger");
-			logger.Info("Hmm.");
-			Thread.Sleep(TimeSpan.FromSeconds(30));
+			logger.Info("Greetings, earthling.");
 		}
 	}
 }
