01e16595fb8e89ecd921b8181ac7afe68a6815ff
maor <maor@logz.io>
2016-12-20 16:02:55 +0200

    Add NLog sample app

---
+++ sample-applications/LogzioNLogSampleApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+using NLog.Fluent;
+
+namespace LogzioNLogSampleApplication
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			var logger = LogManager.GetCurrentClassLogger();
+
+			logger.Info()
+				.Message("Hello to you, from NLog with properties")
+				.Property("userId", "1237123")
+				.Property("sunlocation", "justabovehorizon")
+				.Write();
+		}
+	}
+}
