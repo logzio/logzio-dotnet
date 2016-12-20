76764c77ce18c79d3029a8184e0f1498cc86fdfa
maor <maor@logz.io>
2016-12-20 15:07:45 +0200

    Add a fake NLogSanityTests

---
+++ src/IntegrationTests/NLog/NLogSanityTests.cs
@@ -0,0 +1,32 @@
+﻿using Logzio.DotNet.NLog;
+using NLog;
+using NLog.Config;
+using NUnit.Framework;
+
+namespace Logzio.DotNet.IntegrationTests.NLog
+{
+	[TestFixture]
+	public class NLogSanityTests
+	{
+		[Test]
+		public void Sanity()
+		{
+			var config = new LoggingConfiguration();
+
+			var logzioTarget = new LogzioTarget
+			{
+				Token = "DKJiomZjbFyVvssJDmUAWeEOSNnDARWz",
+				Debug = true,
+				BufferSize = 1,
+				Name = "Logzio"
+			};
+			config.AddTarget("Logzio", logzioTarget);
+			config.AddRule(LogLevel.Debug, LogLevel.Fatal, "Logzio", "*");
+			LogManager.Configuration = config;
+
+			var logger = LogManager.GetCurrentClassLogger();
+			logger.Info("Hello");
+		}
+		 
+	}
+}
