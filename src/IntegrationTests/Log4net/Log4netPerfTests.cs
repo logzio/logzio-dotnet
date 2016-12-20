8c9dfb0edb286f6f7bc9f65a742fc385d868166e
maor <maor@logz.io>
2016-12-20 18:03:30 +0200

    Add perf tests

---
+++ src/IntegrationTests/Log4net/Log4netPerfTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using log4net;
+using log4net.Repository.Hierarchy;
+using Logzio.DotNet.Log4net;
+using NUnit.Framework;
+
+namespace Logzio.DotNet.IntegrationTests.Log4net
+{
+	[TestFixture]
+	public class Log4netPerfTests
+	{
+		[Test]
+		public void Perf()
+		{
+			var hierarchy = (Hierarchy)LogManager.GetRepository();
+			var logzioAppender = new LogzioAppender();
+			logzioAppender.AddToken("DKJiomZjbFyVvssJDmUAWeEOSNnDARWz");
+			logzioAppender.AddBufferSize(100);
+			logzioAppender.AddDebug(true);
+			hierarchy.Root.AddAppender(logzioAppender);
+			hierarchy.Configured = true;
+			var logger = LogManager.GetLogger(typeof (Log4netSanityTests));
+
+			var stopwatch = Stopwatch.StartNew();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				logger.Info("A Fish");
+			}
+
+			stopwatch.Stop();
+			Console.WriteLine("Total time: " + stopwatch.Elapsed);
+			LogManager.Shutdown();
+		}
+	}
+}
---
+++ src/IntegrationTests/NLog/NLogPerfTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Logzio.DotNet.NLog;
+using NLog;
+using NLog.Config;
+using NUnit.Framework;
+
+namespace Logzio.DotNet.IntegrationTests.NLog
+{
+	[TestFixture]
+	public class NLogPerfTests
+	{
+		[Test]
+		public void Perf()
+		{
+			var config = new LoggingConfiguration();
+
+			var logzioTarget = new LogzioTarget
+			{
+				Token = "DKJiomZjbFyVvssJDmUAWeEOSNnDARWz",
+				Debug = true
+			};
+			config.AddTarget("Logzio", logzioTarget);
+			config.AddRule(LogLevel.Debug, LogLevel.Fatal, "Logzio", "*");
+			LogManager.Configuration = config;
+
+			var logger = LogManager.GetCurrentClassLogger();
+
+			var stopwatch = Stopwatch.StartNew();
+
+			for (int i = 0; i < 10000; i++)
+			{
+				logger.Info("A Bird");
+			}
+
+			stopwatch.Stop();
+			Console.WriteLine("Total time: " + stopwatch.Elapsed);
+			LogManager.Shutdown();
+		}
+	}
+}
