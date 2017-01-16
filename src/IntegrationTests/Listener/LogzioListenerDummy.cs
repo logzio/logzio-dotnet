bccfd22ee29e3cae5e7a65d9158e635cfd9fef53
yellowblood <maor@logz.io>
2017-01-16 16:48:31 +0200

    Add missing LogzioListenerDummy.cs

---
+++ src/IntegrationTests/Listener/LogzioListenerDummy.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace Logzio.DotNet.IntegrationTests.Listener
+{
+    public class LogzioListenerDummy
+    {
+        public const string DefaultUrl = "http://127.0.0.1:7621/";
+
+        public IList<string> Requests { get; set; }
+
+        private bool _isActive;
+        private HttpListener _httpListener;
+
+        public void Start(string url = DefaultUrl)
+        {
+            _httpListener = new HttpListener();
+            _httpListener.Prefixes.Add(url);
+            _httpListener.Start();
+            _isActive = true;
+            Requests = new List<string>();
+            _httpListener.BeginGetContext(OnContext, null);
+        }
+
+        private void OnContext(IAsyncResult ar)
+        {
+            var context = _httpListener.EndGetContext(ar);
+
+            if (!_isActive)
+                return;
+
+            _httpListener.BeginGetContext(OnContext, null);
+
+            var request = context.Request;
+            using (var reader = new StreamReader(request.InputStream, request.ContentEncoding))
+            {
+                Requests.Add(reader.ReadToEnd());
+            }
+
+            context.Response.StatusCode = 200;
+            context.Response.Close();
+        }
+
+        public void Stop()
+        {
+            _isActive = false;
+            _httpListener.Close();
+        }
+    }
+}
